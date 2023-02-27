e4cb4d04020b0fe87d1eb23e2c58186a03e76535
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-02-27 22:37:48 +0300

    detected surface

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -22,13 +22,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         // Create a new scene
 //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
 
         // Set the scene to the view
 //        sceneView.scene = scene
         
-        createCube()
+
+//        createDice()
+   
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,6 +40,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        configuration.planeDetection = .horizontal //detect horizontal surface
+        
+    
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -48,26 +54,62 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    func createCube(){
-    
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01) // creating a new brand cube
+  
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {//we take the detected surface as anchor
         
-        let material = SCNMaterial() //created material to give color to our cube
+        if anchor is ARPlaneAnchor{// we need planeAnchor because we want to detect horizontal surface
+            
+            print("plane detected")
+            
+            let planeAnchor = anchor as! ARPlaneAnchor //casting
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))// created plane to fit which detected surface
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)// set node position
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)// we need to transform because detected surface is horizontal but plane is vertical
+            
+            let gridMaterial =  SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            
         
-        material.diffuse.contents = UIColor.red // given color
-        
-        cube.materials = [material]// added color
-        
-        let node = SCNNode() //created a node to see in camera in realworld
-        
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5) //identify position
-        
-        node.geometry = cube //passed cube
-        
-        sceneView.scene.rootNode.addChildNode(node) //add node
-        sceneView.autoenablesDefaultLighting = true
-        
+            node.addChildNode(planeNode)
+            
+        }else{
+            print("plane not detected")
+            return
+        }
         
     }
-   
+        
+
 }
+
+extension ViewController{
+    
+    func createDice(){
+        
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")
+        
+        if let diceNode = diceScene?.rootNode.childNode(withName: "Dice", recursively: true){
+            
+            diceNode.position = SCNVector3(x: 0, y: 0.1, z: -0.1)
+            
+            sceneView.scene.rootNode.addChildNode(diceNode)
+            sceneView.autoenablesDefaultLighting = true
+        }
+        
+        
+    
+    }
+
+}
+
